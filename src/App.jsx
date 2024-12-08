b6aa56295b8c7c3361db000891b8c4f81e1ce9f6
tidiong <62657965+tidiong@users.noreply.github.com>
2024-12-08 14:23:48 -0600

    Update App.jsx
    Add local storage and hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 
 const todoList = [
@@ -5,6 +6,30 @@ const todoList = [
   { id: 2, title: "Study for upcoming Python quiz" },
   { id: 3, title: "Create R&D strategy for wellness project" }
 ];
+=======
+import { useEffect, useState } from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
+
+const useSemiPersistentState = () => {
+
+  const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList") || "[]")
+  const [todoList, setTodoList] = useState(savedTodoList);
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+
+}
+ 
+function App() {
+  
+  const [todoList,setTodoList] = useSemiPersistentState();
+  const addTodo = (newTodo) => {
+    setTodoList((prevList) => [...prevList, newTodo]) };
+>>>>>>> Stashed changes
 
 function App (){
   return(
