50405c58a8d53d7eaf5c3107b05b9843b934491c
tidiong <62657965+tidiong@users.noreply.github.com>
2025-01-07 02:29:28 -0600

    Airtable connect

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,36 @@ const App = () => {
   const [todoTitle, setTodoTitle] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
- 
+  const fetchData = async () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+    },
+  };
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const data = await response.json();
+
+    const todos = data.records.map((record) => ({
+      title: record.fields.title,
+      id: record.id,
+    }));
+
+    setTodoList(todos);
+    setIsLoading(false);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
   useEffect(() => {
-  new Promise((resolve) => {
-    setTimeout(() => {
-      const savedTodos = JSON.parse(localStorage.getItem('todoList') || '[]');
-      resolve(savedTodos);
-    }, 2000);  
-  }).then((fetchedTodos) => {
-    setTodoList(fetchedTodos);
-    setIsLoading(false); 
-  });
-}, []);
+    fetchData();
+  }, []);
 
 
    
