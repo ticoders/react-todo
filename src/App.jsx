0e88c1e6bec87540f5e65315b5a8b160943cb8fb
tidiong <62657965+tidiong@users.noreply.github.com>
2025-02-11 11:05:03 -0600

    Add pagination

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
     },
   };
-  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}?view=Grid%20view&sort[0][field]=title&sort[0][direction]=asc`;
 
   try {
     const response = await fetch(url, options);
